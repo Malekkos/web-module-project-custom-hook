c16e5ce4a9970f10678e1f89b602da54f50f5c91
Christian Anderson <tehninjawaffle@gmail.com>
2023-09-09 17:50:59 -0700

    Application is working as intended, Dark Mode persists through a refresh, and localStorage contains darkMode booleans. I dont know what the false is.

--- frontend/hooks/useDarkMode.js
+++ frontend/hooks/useDarkMode.js
@@ -1,8 +1,8 @@
 import { useState , React} from "react"
-
+import useLocalStorage from "./useLocalStorage"
  
 const useDarkMode = (initialValue) => {
-  const [boolean, setBoolean] = useState(initialValue)
+  const [boolean, setBoolean] = useLocalStorage("darkMode", initialValue)
   const changeBoolean = (value) => {
     setBoolean(!boolean)
   }
